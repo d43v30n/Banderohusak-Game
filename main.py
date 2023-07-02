880303d9c21155f636bbdfa4a13a9c809153fae1
d43v30n <anjey.mail@gmail.com>
2023-07-02 13:19:13 +0200

    Formatting update

--- main.py
+++ main.py
@@ -16,18 +16,19 @@ COLOR_ENEMY = (255, 0, 0)
 COLOR_BONUS = (0, 255, 0)
 SIZE_PLAYER = (20, 20)
 SIZE_ENEMY = (60, 30)
-SIZE_BONUS = (40,100)
-COLOR_BG = (0,0,0)
+SIZE_BONUS = (40, 100)
+COLOR_BG = (0, 0, 0)
 
-main_display = pygame.display.set_mode((WIDTH,HEIGHT))
+main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
-bg = pygame.transform.scale(pygame.image.load('background.png'), 
+bg = pygame.transform.scale(pygame.image.load('background.png'),
                             (WIDTH, HEIGHT))
 bg_x1 = 0
 bg_x2 = bg.get_width()
 bg_move = 3
 
-player = pygame.image.load('player.png').convert_alpha() #pygame.Surface(SIZE_PLAYER)
+# pygame.Surface(SIZE_PLAYER)
+player = pygame.image.load('player.png').convert_alpha()
 player_rect = player.get_rect()
 
 player_move_down = [0, 4]
@@ -38,30 +39,36 @@ player_move_right = [4, 0]
 
 class EnemyClass:
     def init(self):
-        self.event = pygame.USEREVENT +1
+        self.event = pygame.USEREVENT + 1
         pygame.time.set_timer(self.event, 1500)
         self.count = []
         pass
 
     def create_enemy(self):
-        fenemy = pygame.transform.scale(pygame.image.load('enemy.png').convert_alpha(), (SIZE_ENEMY))
-        fenemy_rect = pygame.Rect(WIDTH, random.randint((HEIGHT / 4), (3*HEIGHT/4)), *SIZE_ENEMY)
+        fenemy = pygame.transform.scale(pygame.image.load(
+            'enemy.png').convert_alpha(), (SIZE_ENEMY))
+        fenemy_rect = pygame.Rect(WIDTH, random.randint(
+            (HEIGHT / 4), (3*HEIGHT/4)), *SIZE_ENEMY)
         fenemy_move = [random.randint(-8, -4), 0]
         return [fenemy, fenemy_rect, fenemy_move]
 
+
 class BonusClass:
     def init(self):
-        self.event = pygame.USEREVENT +2
+        self.event = pygame.USEREVENT + 2
         pygame.time.set_timer(self.event, 3000)
         self.count = []
         pass
-        
+
     def create_bonus(self):
-        bonus = pygame.transform.scale(pygame.image.load('bonus.png').convert_alpha(), (SIZE_BONUS))
-        bonus_rect = pygame.Rect(random.randint((WIDTH / 4), (3*WIDTH/4)), 0, *SIZE_BONUS)
+        bonus = pygame.transform.scale(pygame.image.load(
+            'bonus.png').convert_alpha(), (SIZE_BONUS))
+        bonus_rect = pygame.Rect(random.randint(
+            (WIDTH / 4), (3*WIDTH/4)), 0, *SIZE_BONUS)
         bonus_move = [0, random.randint(4, 8)]
         return [bonus, bonus_rect, bonus_move]
-    
+
+
 score = 0
 
 playing = True
@@ -72,10 +79,11 @@ freshbonus = BonusClass()
 while playing:
     FPS.tick(120)
     for event in pygame.event.get():
-        if event.type == QUIT: playing = False
-        if event.type == freshenemy.event: 
+        if event.type == QUIT:
+            playing = False
+        if event.type == freshenemy.event:
             freshenemy.count.append(freshenemy.create_enemy())
-        if event.type == freshbonus.event: 
+        if event.type == freshbonus.event:
             freshbonus.count.append(freshbonus.create_bonus())
 
     keys = pygame.key.get_pressed()
@@ -88,7 +96,7 @@ while playing:
         player_rect = player_rect.move(player_move_left)
     if keys[K_RIGHT] and player_rect.right <= WIDTH:
         player_rect = player_rect.move(player_move_right)
-    
+
     bg_x1 -= bg_move
     bg_x2 -= bg_move
 
@@ -103,17 +111,19 @@ while playing:
     for enemy in freshenemy.count:
         enemy[1] = enemy[1].move(enemy[2])
         main_display.blit(enemy[0], enemy[1])
-        if player_rect.colliderect(enemy[1]): playing = False
+        if player_rect.colliderect(enemy[1]):
+            playing = False
 
     for bonus in freshbonus.count:
         bonus[1] = bonus[1].move(bonus[2])
         main_display.blit(bonus[0], bonus[1])
-        if player_rect.colliderect(bonus[1]): 
+        if player_rect.colliderect(bonus[1]):
             score += 1
             freshbonus.count.pop(freshbonus.count.index(bonus))
 
     main_display.blit(player, player_rect)
-    main_display.blit(FONT.render(str(score), True, COLOR_PLAYER), (WIDTH-50, 20))
+    main_display.blit(FONT.render(str(score), True,
+                      COLOR_PLAYER), (WIDTH-50, 20))
 
     for penemy in freshenemy.count:
         if penemy[1].left < 0:
@@ -122,5 +132,6 @@ while playing:
         if pbonus[1].bottom > HEIGHT:
             freshbonus.count.pop(freshbonus.count.index(pbonus))
 
-    if keys[K_ESCAPE]: break
+    if keys[K_ESCAPE]:
+        break
     pygame.display.flip()
