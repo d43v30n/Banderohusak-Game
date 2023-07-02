72cd51097c4773fdf4fbdc8b419ceb6af1686b99
d43v30n <anjey.mail@gmail.com>
2023-07-02 12:34:21 +0200

    Classes for enemies

--- main_day3.5.py
+++ main_day3.5.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import os
 from pygame.constants import QUIT, K_DOWN, K_LEFT, K_UP, K_RIGHT, K_ESCAPE
 
 pygame.init()
@@ -18,6 +19,9 @@ SIZE_PLAYER = (20, 20)
 SIZE_ENEMY = (60, 30)
 SIZE_BONUS = (40, 100)
 COLOR_BG = (0, 0, 0)
+
+IMAGE_PATH = "Goose"
+PLAYER_IMAGES = os.listdir(IMAGE_PATH)
 
 main_display = pygame.display.set_mode((WIDTH,HEIGHT))
 
@@ -61,12 +65,14 @@ CREATE_ENEMY = pygame.USEREVENT +1
 pygame.time.set_timer(CREATE_ENEMY, 1500)
 CREATE_BONUS = pygame.USEREVENT +2
 pygame.time.set_timer(CREATE_BONUS, 3000)
+CHANGE_IMAGE = pygame.USEREVENT + 3
+pygame.time.set_timer(CHANGE_IMAGE, 250)
+image_index = 0
 enemies = []
 bonuses = []
-
 score = 0
-
 playing = True
+image_flag = 1
 
 while playing:
     FPS.tick(120)
@@ -74,7 +80,14 @@ while playing:
         if event.type == QUIT: playing = False
         if event.type == CREATE_ENEMY: enemies.append(create_enemy())
         if event.type == CREATE_BONUS: bonuses.append(create_bonus())
-
+        if event.type == CHANGE_IMAGE:
+            player = pygame.image.load(
+                os.path.join(IMAGE_PATH, PLAYER_IMAGES[image_index])
+                ).convert_alpha()
+            image_index = image_index + image_flag
+            if image_index == len(PLAYER_IMAGES) or image_index == 0: 
+                image_flag = image_flag * -1
+    
     keys = pygame.key.get_pressed()
 
     if keys[K_DOWN] and player_rect.bottom <= HEIGHT:
@@ -85,7 +98,7 @@ while playing:
         player_rect = player_rect.move(player_move_left)
     if keys[K_RIGHT] and player_rect.right <= WIDTH:
         player_rect = player_rect.move(player_move_right)
-    
+    print(image_index, image_flag)
     bg_x1 -= bg_move
     bg_x2 -= bg_move
 
